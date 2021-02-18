64f573291f142738116bf1c38fece5f5986857d1
jannthorbengraef <jann-thorben.graef@deutschebahn.com>
2021-02-18 13:44:38 +0100

    refactor logging and error check

--- register_read/decrypt/decrypter_windows_386.go
+++ register_read/decrypt/decrypter_windows_386.go
@@ -2,60 +2,49 @@ package decrypter
 
 import (
 	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"path"
+	error_log "registryread/error"
 
 	"github.com/billgraziano/dpapi"
 )
 
 // Decrypt will unprotect []byte with the DPAPI in context of the current user
 func Decrypt(key []byte) []byte {
-	log.SetFlags(0)
-	log.SetPrefix("decrypter: ")
 	decryptedbin, err := dpapi.DecryptBytes(key)
-	check(err, "Unprotect String with DPAPI")
+	error_log.Check(err, "Unprotect String with DPAPI", "decrypter")
 	return decryptedbin
 }
 
 // IpSecProcDll will unprotect a []byte with the ipsecproc.dll of windows OS
 func IpSecProcDll(key []byte) []byte {
-	log.SetFlags(0)
-	log.SetPrefix("decrypter: ")
 	// Create temp files and directories
 	dir, err := ioutil.TempDir("./decrypt", "temp")
-	check(err, "create Directory: "+dir)
+	error_log.Check(err, "create Directory: "+dir, "decrypter")
 	file, err := ioutil.TempFile(dir, "sk_unencrypted")
-	check(err, "creating Temp File")
+	error_log.Check(err, "creating Temp File", "decrypter")
 	_, err = file.Write(key)
-	check(err, "create sk-encrypted file")
+	error_log.Check(err, "create sk-encrypted file", "decrypter")
 	// execute decrypter.exe
 	exec_decrypt := exec.Command("powershell.exe", `.\decrypt\decrypter.exe`, `.\`+file.Name()+" "+`.\`+dir+`\sk-rsa-decrypted.dat`)
 	err = exec_decrypt.Run()
-	check(err, "execute decrypter.exe")
+	error_log.Check(err, "execute decrypter.exe", "decrypter")
 	// Read decrypted sk file
 	sk_decrypted, err := ioutil.ReadFile(`.\` + dir + `\sk-rsa-decrypted.dat`)
-	check(err, "read decrytped sk file")
+	error_log.Check(err, "read decrytped sk file", "decrypter")
 	file.Close()
 	//delte temp
 	remove_dir_content(dir)
 	return sk_decrypted
 }
 
-func check(e error, s string) {
-	if e != nil {
-		log.Println(s)
-		log.Fatal(e)
-	}
-}
-
 func remove_dir_content(dir string) {
 	files, err := ioutil.ReadDir(dir)
-	check(err, "delte temp files")
+	error_log.Check(err, "delte temp files", "decrypter")
 	for _, d := range files {
 		os.RemoveAll(path.Join([]string{dir, d.Name()}...))
 	}
 	err = os.Remove(dir)
-	check(err, "delte temp directory")
+	error_log.Check(err, "delte temp directory", "decrypter")
 }
--- register_read/read/register_read_windows_386.go
+++ register_read/read/register_read_windows_386.go
@@ -1,7 +1,7 @@
 package read
 
 import (
-	"log"
+	error_log "registryread/error"
 
 	"golang.org/x/sys/windows/registry"
 )
@@ -11,27 +11,19 @@ import (
 
 // OpenMkSk extracts the mk and sk value for a specific user and returns an error if needed
 func OpenMkSK() ([]byte, []byte) {
-	log.SetFlags(0)
-	log.SetPrefix("open_mk_sk: ")
-
 	// Open registry Key
 	msippKey, err := registry.OpenKey(registry.CURRENT_USER, `Software\Classes\Local Settings\Software\Microsoft\MSIPC`, registry.QUERY_VALUE)
 	if err != nil {
-		log.Fatal(err)
+		error_log.Check(err, "cant open registry", "open_mk_sk")
 		msippKey.Close()
 	}
 
 	// Read MSIPP-MK Value from Registry Key
 	msippMkBin, _, err := msippKey.GetBinaryValue("MSIPP-MK")
-	if err != nil {
-		log.Fatal(err)
-	}
+	error_log.Check(err, "cant read MK Value", "open_mk_sk")
 
 	//read MSIPP-SK Value from Registry Key
 	msippSkBin, _, err := msippKey.GetBinaryValue("MSIPP-SK")
-	if err != nil {
-		log.Fatal(err)
-	}
+	error_log.Check(err, "cant read SK Value", "open_mk_sk")
 	return msippSkBin, msippMkBin
-
 }
